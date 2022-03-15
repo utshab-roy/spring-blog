5761e3853f56d5103e344321c6d655263cd6fca6
Utshab Roy <utshab.roy@gmail.com>
2022-03-15 18:09:30 +0900

    basic setup for registration, env value change

--- src/main/java/com/utshab/blog/appuser/AppUserService.java
+++ src/main/java/com/utshab/blog/appuser/AppUserService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class UserService implements UserDetailsService {
+public class AppUserService implements UserDetailsService {
     private final static String USER_NOT_FOUND_MSG = "user with email　%s not found !";
     private final AppUserRepository appUserRepository;
 
---
+++ src/main/java/com/utshab/blog/registration/RegistrationController.java
@@ -0,0 +1,21 @@
+package com.utshab.blog.registration;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(path = "api/v1/registration")
+public class RegistrationController {
+
+    private RegistrationService registrationService;
+
+    @PostMapping
+    public String register(@RequestBody RegistrationRequest request)  {
+        return  registrationService.register(request);
+    }
+ }
