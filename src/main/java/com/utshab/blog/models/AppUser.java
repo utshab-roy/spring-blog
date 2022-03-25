c7e1ddc41e2e9041fc8311fdd52efa0caa642a94
Utshab Roy <utshab.roy@gmail.com>
2022-03-25 17:38:59 +0900

    entity class created for user and role

---
+++ src/main/java/com/utshab/blog/models/AppUser.java
@@ -0,0 +1,25 @@
+package com.utshab.blog.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String username;
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER) // we will load the role everytime when we load the user
+    private Collection<Role> roles = new ArrayList<>();
+
+}
